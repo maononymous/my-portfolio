4465ca07d2ab07f0e894150c6b37b080efb52f09
Abdullah Omer Mohammed <mabdul23@depaul.edu>
2025-07-08 14:48:37 -0500

    Initial Section setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import ModeToggleButton from './components/ModeToggleButton.jsx'
+import Section1 from './components/Section1.jsx'
 
 function App() {
   const [mode, setMode] = useState('DNA');
@@ -10,9 +11,7 @@ function App() {
   return (
     <div>
       <ModeToggleButton mode={mode} setMode={setMode} />
-      <main style={{ height: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-        <h1>{mode === 'DNA' ? 'Welcome to the DNA View 🧬' : 'Welcome to the Planet View 🌍'}</h1>
-      </main>
+      <Section1 mode={mode} />
     </div>
   )
 }
