90169bbbefee87ffbc0adfcc97c7f3853d2c904e
Abdullah Omer Mohammed <mabdul23@depaul.edu>
2025-07-13 11:40:40 -0500

    Background upgrade

--- src/App.jsx
+++ src/App.jsx
@@ -33,7 +33,7 @@ const App = () => {
         setCurrentIndex(nextIndex)
         setPlanetId((prev) => (prev % 5) + 1)
         setIsCloudVisible('exit')
-      }, 600)
+      }, 800)
 
       setTimeout(() => {
         setIsCloudVisible(false)
